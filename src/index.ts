c055602f7b9bbc9a36cff56c8a3a953c5479e4a0
David Sargeant <david@dsargeant.com>
2024-03-15 14:34:15 -0400

    First pass at replacements.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import { isMatch } from 'lodash'
 import * as htmlparser2 from 'htmlparser2'
+import _debug from 'debug'
 
-type Options = {
+const debug = _debug('extract-text-html')
+
+export interface Replacement {
+  /** Tag name to match (without brackets) */
+  matchTag: string
+  /** Attributes to match */
+  matchAttrs?: Record<string, string>
+  /** Text to replace the tag with */
+  text: string
+  /** Is the tag self-closing?  */
+  selfClosing?: boolean
+}
+
+export interface Options {
   /** Exclude the content from the set of tags. For example, style and script. */
   excludeContentFromTags?: string[]
   /** Reduces multiple spaces to a single space and trims whitespace from the start and end. */
   trimWhitespace?: boolean
+  /** Replace a tag with some text. Flag self-closing tags with selfClosing: true. */
+  replacements?: Replacement[]
 }
 
 // Exclude content from metadata tags.
@@ -29,16 +46,34 @@ export const extractText = (html: string, options: Options = {}) => {
   const excludeTags =
     options.excludeContentFromTags ?? defaultExcludeContentFromTags
   const trimWhitespace = options.trimWhitespace ?? true
+  const replacements = options.replacements ?? []
 
   let excludeText = false
   let strippedText = ''
+  let replacement: Replacement | undefined
 
   const shouldExclude = (name: string) => excludeTags.includes(name)
 
+  const findReplacement = (name: string, attrs: Record<string, string> = {}) =>
+    replacements.find(({ matchTag, matchAttrs }) => {
+      if (matchTag === name) {
+        if (matchAttrs) {
+          return isMatch(attrs, matchAttrs)
+        }
+        return true
+      }
+    })
+
   const parser = new htmlparser2.Parser({
-    onopentagname(name) {
+    onopentag(name, attrs) {
+      debug('open tag name %s', name)
       if (shouldExclude(name)) {
         excludeText = true
+      }
+      replacement = findReplacement(name, attrs)
+      if (options.replacements && replacement) {
+        debug('replace open tag %s with attrs %o', name, attrs)
+        strippedText += replacement.text
       }
     },
     ontext(text) {
@@ -47,8 +82,13 @@ export const extractText = (html: string, options: Options = {}) => {
       }
     },
     onclosetag(name) {
+      debug('close tag name %s', name)
       if (shouldExclude(name)) {
         excludeText = false
+      }
+      if (options.replacements && replacement && !replacement.selfClosing) {
+        debug('replace close tag %s with %s', name, replacement.text)
+        strippedText += replacement.text
       }
     },
   })
